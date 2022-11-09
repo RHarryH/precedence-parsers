c62a1cf229d6a398862c5fbdc288aa3255594f64
Rafał Hiszpański <rharryh@gmail.com>
2022-11-09 23:43:31 +0100

    Added logging Quick refactor Temporarily disabled functions and unary operators support

--- src/main/java/com/avispa/precedence_parsers/shunting_yard/ShuntingYard.java
+++ src/main/java/com/avispa/precedence_parsers/shunting_yard/ShuntingYard.java
@@ -1,94 +1,94 @@
 package com.avispa.precedence_parsers.shunting_yard;
 
 import java.text.ParseException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Stack;
 
 import com.avispa.precedence_parsers.shunting_yard.token.Function;
 import com.avispa.precedence_parsers.shunting_yard.token.Misc;
 import com.avispa.precedence_parsers.shunting_yard.token.Operator;
 import com.avispa.precedence_parsers.shunting_yard.token.Token;
+import lombok.extern.slf4j.Slf4j;
 
-public class ReversePolishNotation {
+@Slf4j
+public class ShuntingYard {
+    private int argsCount = 1;
     
-    private String expression;
-    private List<Object> rpn = new ArrayList<>();
-    private Stack<Token> stack = new Stack<>();
-    
-    private Integer argsCount = 1;
+    public String parse(String expression) {
+		List<Object> output = new ArrayList<>();
+		Deque<Token> stack = new ArrayDeque<>();
 
-    public ReversePolishNotation(String expression) {
-        expression = expression.replace(" ", "").replace("(-", "(0-")
-                .replace(",-", ",0-");
-        if(expression.startsWith("-("))
-        	expression = "-1*(" + expression.substring(2);
-        if(expression.charAt(0) == '-')
-            expression = "0" + expression;
-        
-        this.expression = expression;
+		//expression = unaryPreprocess(expression);
 
-    }
-    
-    public String getRPN() {
-        return rpn.toString();
-    }
-    
-    public Boolean parse() {
         StringBuilder parsingExpression = new StringBuilder(expression);
-        rpn.clear();
         while(parsingExpression.length() > 0) {
         	try {
-        		lookForTokens(parsingExpression);
+        		lookForTokens(parsingExpression, stack, output);
         	} catch(ParseException e) {
-        		System.err.println(e.toString());
-        		rpn.clear();
-        		return false;
+        		log.error(e.toString());
+        		return "";
         	}
         }
         
-        while(!stack.isEmpty()) 
-        	rpn.add(stack.pop());
+        while(!stack.isEmpty()) {
+			output.add(stack.pop());
+		}
         
-        return true;
+        return output.toString();
     }
-    
-    private void lookForTokens(StringBuilder sb) throws ParseException {
 
-        unaryMinus();
+	/*private String unaryPreprocess(String expression) {
+		expression = expression.replace(" ", "").replace("(-", "(0-")
+				.replace(",-", ",0-");
+		if(expression.startsWith("-(")) {
+			expression = "-1*(" + expression.substring(2);
+		}
+		if(expression.charAt(0) == '-') {
+			expression = "0" + expression;
+		}
+		return expression;
+	}*/
+    
+    private void lookForTokens(StringBuilder sb, Deque<Token> stack, List<Object> output) throws ParseException {
+        /*unaryMinus(stack, output);
         
         if(!stack.isEmpty() && stack.peek() instanceof Function) {
         	if(!sb.toString().startsWith(Misc.LEFT_PARENTHESIS.getSymbol()))
         		throw new ParseException("Parenthesis is expected after function name", 0);
-        }
+        }*/
 
-        if(!processFunctions(sb) && !processOperators(sb)) {
-        	Integer symbolLength = 1;
+        if(/*!processFunctions(sb, stack) &&*/ !processOperators(sb, stack, output)) {
+        	int symbolLength = 1;
         	
         	if(sb.toString().startsWith(Misc.COMMA.getSymbol())) {
         		while(stack.peek() != Misc.LEFT_PARENTHESIS) {
-        			rpn.add(stack.pop());
-        			if(stack.empty())
-        				throw new ParseException("Missing left parenthesis", 0);
+        			output.add(stack.pop());
+        			if(stack.isEmpty()) {
+						throw new ParseException("Missing left parenthesis", 0);
+					}
 	        	}
         		argsCount++;
         	} else if(sb.toString().startsWith(Misc.LEFT_PARENTHESIS.getSymbol()))
 	        	stack.push(Misc.LEFT_PARENTHESIS);
         	else if(sb.toString().startsWith(Misc.RIGHT_PARENTHESIS.getSymbol())) {
 	        	while(stack.peek() != Misc.LEFT_PARENTHESIS) {
-	                rpn.add(stack.pop());
-	                if(stack.empty())
-	                	throw new ParseException("Missing left parenthesis", 0);
+	                output.add(stack.pop());
+	                if(stack.isEmpty()) {
+						throw new ParseException("Missing left parenthesis", 0);
+					}
 	        	}
 	            stack.pop(); // pop left parenthesis
 	            
 	            if(!stack.isEmpty() && stack.peek() instanceof Function) {
 	            	Function function = (Function)stack.pop();
-	            	if(!function.getMaxArgs().equals(argsCount))
-	            		throw new ParseException("Incorrect arguments count for function " + function.getSymbol() + ". Expected count: " + function.getMaxArgs(), 0);
+	            	if(function.getMaxArgs() != argsCount) {
+						throw new ParseException("Incorrect arguments count for function " + function.getSymbol() + ". Expected count: " + function.getMaxArgs(), 0);
+					}
 	            	argsCount = 1;
-	            	rpn.add(function);
+	            	output.add(function);
 	            }
 	        } else {
 	        	StringBuilder variable = new StringBuilder();
@@ -101,7 +101,7 @@ public class ReversePolishNotation {
 	            		break;
 	                    
 	            }
-	            rpn.add(variable.toString());
+	            output.add(variable.toString());
 	            symbolLength = variable.length();
 	        }
         	
@@ -109,20 +109,17 @@ public class ReversePolishNotation {
         }
     }
 
-	/**
-	 * 
-	 */
-	private void unaryMinus() {
-		if(rpn.size() >= 2) {
-    		if(!stack.isEmpty() && stack.peek().equals(Operator.SUBTRACT) && rpn.get(rpn.size() - 2).equals("0") ) {
+	/*private void unaryMinus(Deque<Token> stack, List<Object> output) {
+		if(output.size() >= 2) {
+    		if(!stack.isEmpty() && stack.peek().equals(Operator.SUBTRACT) && output.get(output.size() - 2).equals("0") ) {
     			stack.pop();
-    			rpn.remove(rpn.size() - 2);
-    			rpn.set(rpn.size() - 1, "-" + (String) rpn.get(rpn.size() - 1));
+    			output.remove(output.size() - 2);
+    			output.set(output.size() - 1, "-" + output.get(output.size() - 1));
     		}
     	}
-	}
+	}*/
     
-    private Boolean processFunctions(StringBuilder sb) {
+    /*private boolean processFunctions(StringBuilder sb, Deque<Token> stack) {
     	// functions
     	for(Function function : Function.values()) {
     		if(sb.toString().startsWith(function.getSymbol())) {
@@ -133,24 +130,20 @@ public class ReversePolishNotation {
     	}
     
     	return false;
-    }
+    }*/
 
-	/**
-	 * @param sb
-	 * @param symbolLength
-	 * @return
-	 */
-	private Boolean processOperators(StringBuilder sb) {
+	private boolean processOperators(StringBuilder sb, Deque<Token> stack, List<Object> output) {
 		for(Operator operator : Operator.values()) {
         	if(sb.toString().startsWith(operator.getSymbol())) {
             	while(!stack.isEmpty() && stack.peek() instanceof Operator) {
         			Operator top = (Operator) stack.peek();
 
-                    if((operator.getSyntaxAssociativity().equals(SyntaxAssociativity.LEFT) && operator.getPriority() <= top.getPriority()) ||
-                       (operator.getSyntaxAssociativity().equals(SyntaxAssociativity.RIGHT) && operator.getPriority() < top.getPriority()))
-                    	rpn.add(stack.pop());
-                    else 
-                    	break;
+                    if((operator.isLeftAssociative() && operator.getPrecedence() <= top.getPrecedence()) ||
+                       (operator.isRightAssociative() && operator.getPrecedence() < top.getPrecedence())) {
+						output.add(stack.pop());
+					} else {
+						break;
+					}
                 }
             	stack.push(operator); 
             	sb = sb.delete(0, operator.getSymbol().length());
@@ -164,7 +157,7 @@ public class ReversePolishNotation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Put expression: ");
+        log.info("Put expression: ");
         
         Scanner scanner = new Scanner(System.in);
         
@@ -172,9 +165,7 @@ public class ReversePolishNotation {
         
         scanner.close();
         
-        ReversePolishNotation reversePolishNotation = new ReversePolishNotation(expression);
-        
-        if(reversePolishNotation.parse())
-        	System.out.println(reversePolishNotation.getRPN());
+        ShuntingYard shuntingYard = new ShuntingYard();
+		log.info(shuntingYard.parse(expression));
     }
 }
--- src/main/java/com/avispa/precedence_parsers/shunting_yard/token/Misc.java
+++ src/main/java/com/avispa/precedence_parsers/shunting_yard/token/Misc.java
@@ -1,20 +1,17 @@
 package com.avispa.precedence_parsers.shunting_yard.token;
 
+import lombok.Getter;
+
+@Getter
 public enum Misc implements Token {
 	COMMA(","),
 	PERCENT("%"),
 	LEFT_PARENTHESIS("("),
 	RIGHT_PARENTHESIS(")");
-	
+
 	private final String symbol;
 	
 	Misc(final String symbol) {
 		this.symbol = symbol;
 	}
-
-	@Override
-	public String getSymbol() {
-		return symbol;
-	}
-
 }

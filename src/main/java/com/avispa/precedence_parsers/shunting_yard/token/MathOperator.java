06172befafda29b5d84a62b6f310d57c5bfdb41b
Rafał Hiszpański <rharryh@gmail.com>
2022-11-10 15:55:03 +0100

    Reworked functions support

--- src/main/java/com/avispa/precedence_parsers/shunting_yard/token/MathOperator.java
+++ src/main/java/com/avispa/precedence_parsers/shunting_yard/token/MathOperator.java
@@ -1,6 +1,6 @@
 package com.avispa.precedence_parsers.shunting_yard.token;
 
-public interface Operator extends Token {
+public interface MathOperator extends Token {
 	int getPrecedence();
 
 	boolean isLeftAssociative();

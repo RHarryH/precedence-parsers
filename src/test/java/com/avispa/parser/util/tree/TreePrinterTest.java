f0df9227a7b671102ef9366c95dd1723fd00cba3
Rafał Hiszpański <rharryh@gmail.com>
2022-11-12 12:11:53 +0100

    Extended tree printer tests

--- src/test/java/com/avispa/parser/util/tree/TreePrinterTest.java
+++ src/test/java/com/avispa/parser/util/tree/TreePrinterTest.java
@@ -32,8 +32,7 @@ class TreePrinterTest {
         TreeNode<Token> root = new TreeNode<>(BinaryOperatorToken.ADD);
         root.addChild(new TreeNode<>(FunctionToken.MAX));
 
-        String expected =
-                "ADD" + NEW_LINE +
+        String expected = "ADD" + NEW_LINE +
                 "└── MAX" + NEW_LINE;
 
         assertEquals(expected, TreePrinter.print(root));
@@ -45,8 +44,7 @@ class TreePrinterTest {
         root.addChild(new TreeNode<>(FunctionToken.MAX));
         root.addChild(new TreeNode<>(UnaryOperatorToken.PLUS));
 
-        String expected =
-                "ADD" + NEW_LINE +
+        String expected = "ADD" + NEW_LINE +
                 "├── MAX" + NEW_LINE +
                 "└── PLUS" + NEW_LINE;
 
@@ -61,8 +59,7 @@ class TreePrinterTest {
         root.addChild(child);
         child.addChild(new TreeNode<>(FunctionToken.MODULO));
 
-        String expected =
-                "ADD" + NEW_LINE +
+        String expected = "ADD" + NEW_LINE +
                         "├── MAX" + NEW_LINE +
                         "└── PLUS" + NEW_LINE +
                         "    └── MODULO" + NEW_LINE;
@@ -79,8 +76,7 @@ class TreePrinterTest {
 
         root.addChild(new TreeNode<>(UnaryOperatorToken.PLUS));
 
-        String expected =
-                "ADD" + NEW_LINE +
+        String expected = "ADD" + NEW_LINE +
                         "├── MAX" + NEW_LINE +
                         "│   └── MODULO" + NEW_LINE +
                         "└── PLUS" + NEW_LINE;
@@ -99,12 +95,42 @@ class TreePrinterTest {
 
         root.addChild(new TreeNode<>(UnaryOperatorToken.PLUS));
 
-        String expected =
-                "ADD" + NEW_LINE +
+        String expected = "ADD" + NEW_LINE +
                         "├── MAX" + NEW_LINE +
                         "│   └── MODULO" + NEW_LINE +
                         "│       └── PLUS" + NEW_LINE +
                         "└── PLUS" + NEW_LINE;
+
+        assertEquals(expected, TreePrinter.print(root));
+    }
+
+    @Test
+    void givenNestingInTwoNodes_whenPrint_thenCorrectPointersAreUsed() {
+        TreeNode<Token> root = new TreeNode<>(BinaryOperatorToken.ADD);
+        TreeNode<Token> child1 = new TreeNode<>(FunctionToken.MAX);
+        root.addChild(child1);
+        TreeNode<Token> nestedChild1 = new TreeNode<>(FunctionToken.MODULO);
+        nestedChild1.addChild(new TreeNode<>(UnaryOperatorToken.PLUS));
+        child1.addChild(nestedChild1);
+
+        TreeNode<Token> child2 = new TreeNode<>(FunctionToken.SQRT);
+        root.addChild(child2);
+        TreeNode<Token> nestedChild2 = new TreeNode<>(Operand.from("2"));
+        nestedChild2.addChild(new TreeNode<>(BinaryOperatorToken.DIVIDE));
+        child2.addChild(nestedChild2);
+        child2.addChild(new TreeNode<>(Operand.from("3")));
+
+        root.addChild(new TreeNode<>(UnaryOperatorToken.PLUS));
+
+        String expected = "ADD" + NEW_LINE +
+                "├── MAX" + NEW_LINE +
+                "│   └── MODULO" + NEW_LINE +
+                "│       └── PLUS" + NEW_LINE +
+                "├── SQRT" + NEW_LINE +
+                "│   ├── 2" + NEW_LINE +
+                "│   │   └── DIVIDE" + NEW_LINE +
+                "│   └── 3" + NEW_LINE +
+                "└── PLUS" + NEW_LINE;
 
         assertEquals(expected, TreePrinter.print(root));
     }

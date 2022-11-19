9ee719b24a10e034cb2d4289f3194f3bc22c3e21
Rafał Hiszpański <rharryh@gmail.com>
2022-11-19 21:26:58 +0100

    Optimized imports

--- src/test/java/com/avispa/parser/misc/TablePrinterTest.java
+++ src/test/java/com/avispa/parser/misc/TablePrinterTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Rafał Hiszpański
